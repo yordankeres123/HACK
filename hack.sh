c87a11fa57fba16b24ba8f2ba508cd3e765e2ff8
Bigbos starla <48884075+yordankeres123@users.noreply.github.com>
2019-04-25 20:26:27 +0700

    Add files via upload

---
+++ hack.sh
@@ -0,0 +1,67 @@
+clear
+figlet hack | lolcat
+echo
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo " author BIGBOS STARLA" | lolcat
+echo " team CYB3R POP1CE +62" | lolcat
+echo " FRIENDS ALL CYB3R POP1CE " | lolcat
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■" | lolcat
+echo
+echo " DAFTAR MENU" | lolcat
+echo
+echo " 1. HACK FACEBOOK" | lolcat
+echo " 2. HACK INSTRAGRAM"
+echo " 0. EXIT / KLUAR" | lolcat
+echo
+echo " Silahkan dipilih"
+echo "Pilih 1 jangan 01" | lolcat
+echo "pilih 2 jangan 02 dan seterusnya"
+read -p "PILIHAN~##" cok
+
+if [ $cok = 1 ] || [ $cok = 01 ]
+then
+clear
+figlet HACK | lolcat
+echo
+echo "GUE CUMA INGIN BILANG" | lolcat
+echo "JANGAN DIBUAT KEJAHATAN"
+echo " Tunggu 3 detik" | lolcat
+sleep 3
+echo "GUE CUMA MAU BILANG" | lolcat
+echo "GUE GANTENG KAN"
+echo "YANG NO 2 ASLI WORK" | lolcat
+sleep 2
+cd HACK
+sh hack.sh
+fi
+
+if [ $cok = 2 ] || [ $cok = 02 ]
+then
+clear
+figlet HACK | lolcat
+echo                                                       echo "GUE CUMA INGIN BILANG" | lolcat
+echo "JANGAN DIBUAT KEJAHATAN"
+echo " Tunggu 3 detik" | lolcat                            sleep 3
+echo "GUE CUMA MAU BILANG" | lolcat
+echo "GUE GANTENG KAN"
+echo "YANG NO 1 ASLI WORK" | lolcat
+sleep 2
+cd HACK
+sh hack.sh
+fi
+
+if [ $cok = 0 ] || [ $cok = 00 ]
+then
+clear
+toilet MAKASIH | lolcat
+echo
+echo " MAKASIH ANDA TELAH KENA" | lolcat
+echo
+echo "	PRANK	"
+echo " TUNGGU 20 DETIK ATAU DATA DATA ANDA"
+echo " AKAN HILANG HEHEHHEHEHEHE" | lolcat
+echo " JANGAN DIEXIT ATAU AKAN HILANG DATAMU"
+sleep 20
+exit
+fi
+issijsjsj
